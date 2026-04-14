b7f37281d822d3b21a8415974e129ea3d308a785
franciscopedrodevtest24 <francisco.pedro@devtest.co.ao>
2026-04-15 00:04:17 +0100

    Refactor server initialization to use Bun.serve with updated configuration and remove commented-out code for clarity.

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
 import { start_matches_cron } from "./modules/matches/matches.cron";
 import app from "./server/app";
 
-const SERVER_CONFIG={
-  port:parseInt(Bun.env.PORT||"3001"),
-  hostname:"0.0.0.0"
-}
+// const SERVER_CONFIG={
+//   port:parseInt(Bun.env.PORT||"3001"),
+//   hostname:"0.0.0.0"
+// }
 
 
-const initial_app=app.listen({
-  port:SERVER_CONFIG.port,
-  hostname:SERVER_CONFIG.hostname,
-  idleTimeout:65
+// const initial_app=app.listen({
+//   port:SERVER_CONFIG.port,
+//   hostname:SERVER_CONFIG.hostname,
+//   idleTimeout:65
+// })
+
+
+
+Bun.serve({
+  port:3000,
+  hostname:"0.0.0.0",
+  idleTimeout:65,
+  fetch:async(req)=>{
+   console.log(`SERVER RUNNING  http://localhost:3000`)
+        start_matches_cron()
+
+    return await app.handle(req)
+  },
+
 })
 
 
-if(initial_app.server){
-  console.log(`SERVER RUNNING  http://localhost:${SERVER_CONFIG.port}`)
-  start_matches_cron()
-}else{
-  console.error("ERROR SERVER")
-  process.exit()
-}
 
-export default initial_app
